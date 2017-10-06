90ab79134efab3dbdfd9bd34e3ff26ccb54e674a
Henri Gomez <henri.gomez@sonarsource.com>
2017-10-06 16:14:13 +0200

    Implement upload service #5

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,21 +1,34 @@
 extern crate iron;
 extern crate params;
+extern crate multipart;
 
 
 use iron::prelude::*;
 use std::path::Path;
 use std::fs::File;
+use std::fs;
+use iron::status;
+use self::multipart::server::{Multipart, Entries, SaveResult};
 
 use hyper::header::{ContentDisposition, DispositionType, DispositionParam, Charset};
 
-pub fn hello_handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((iron::status::Ok, "Hello World")))
+/*
+ Welcome Handler
+*/
+pub fn welcome_handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Welcome to Teddy, see ya !")))
 }
 
+/*
+ PingHandler
+*/
 pub fn ping_handler(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok, "pong")))
 }
 
+/*
+ Download Handler
+*/
 pub fn download_handler(req: &mut Request) -> IronResult<Response> {
     use self::params::{Params, Value};
 
@@ -39,3 +52,83 @@ pub fn download_handler(req: &mut Request) -> IronResult<Response> {
         _ => Ok(Response::with(iron::status::NotFound)),
     }
 }
+
+
+/*
+ Upload Handler
+*/
+pub fn upload_handler(request: &mut Request) -> IronResult<Response> {
+
+//   let map = req.get_ref::<Params>().unwrap();
+//    match map.find(&["location"]) {
+//    }
+
+    // Getting a multipart reader wrapper
+    match Multipart::from_request(request) {
+        Ok(mut multipart) => {
+            // Fetching all data and processing it.
+            // save().temp() reads the request fully, parsing all fields and saving all files
+            // in a new temporary directory under the OS temporary directory.
+            match multipart.save().temp() {
+
+                SaveResult::Full(entries) => process_entries(entries),
+                SaveResult::Partial(entries, reason) => {
+                    process_entries(entries.keep_partial())?;
+                    Ok(Response::with((
+                        status::BadRequest,
+                        format!("error reading request: {}", reason.unwrap_err())
+                    )))
+                }
+                SaveResult::Error(error) => Ok(Response::with((
+                    status::BadRequest,
+                    format!("error reading request: {}", error)
+                ))),
+            }
+        }
+        Err(_) => {
+            Ok(Response::with((status::BadRequest, "The request is not multipart")))
+        }
+    }
+}
+
+/// Processes saved entries from multipart request.
+/// Returns an OK response or an error.
+fn process_entries(entries: Entries) -> IronResult<Response> {
+
+    let mut destination = String::new();
+
+    for (name, field) in entries.fields {
+        println!("Field {:?}: {:?}", name, field);
+
+        if name == "destination" {
+           destination = field.to_string();
+        }
+    }
+
+    for (name, files) in entries.files {
+        println!("Field {:?} has {} files:", name, files.len());
+
+        for file in files {
+            let oldpath = format!("{:?}", file.path).replace("\"", "");
+            let newdir = format!("{:?}", destination).replace("\"", "");
+            let newpath = format!("{:?}/{:?}", destination, file.filename.unwrap()).replace("\"", "");
+
+            println!("upload destination will be {}", newpath);
+
+            match fs::create_dir(newdir) {
+                Err(why) => println!("create_dir failed {:?}", why.kind()),
+                Ok(_) => {},
+            }
+
+            println!("Uploaded file moved from {} to {}", oldpath, newpath);
+
+            match fs::rename(oldpath, newpath ) {
+                Err(why) => println!("rename failed {:?}", why.kind()),
+                Ok(_) => {},
+            }
+        }
+    }
+
+    Ok(Response::with((status::Ok, "Multipart data is processed")))
+}
+
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use iron::prelude::*;
 use router::Router;
 
 mod handlers;
-use handlers::{hello_handler, ping_handler, download_handler};
+use handlers::{welcome_handler, ping_handler, download_handler, upload_handler};
 
 mod auth;
 use auth::AuthChecker;
@@ -27,14 +27,15 @@ fn main() {
     let conf = get_config();
 
     let mut router = Router::new();
+    router.get("/", welcome_handler, "welcome");
     router.get("/ping", ping_handler, "ping");
-    router.get("/hello", hello_handler, "hello");
     router.get("/download", download_handler, "download");
+    router.post("/upload", upload_handler, "upload");
 
     let mut chain = Chain::new(router);
     chain.link_before(ResponseTime);
     chain.link_before(AuthChecker::new(&conf.authentication));
     chain.link_after(ResponseTime);
 
-    Iron::new(chain).http(&get_address(conf)).unwrap();
+    Iron::new(chain).http(&get_address(conf)).expect("Failed to start HTTP server");
 }
